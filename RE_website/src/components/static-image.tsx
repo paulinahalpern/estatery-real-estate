26321bdde9d37fff483834717dbdad72ca96e002
Paulina Kaczmarek <kaczmarekpaulina@yahoo.com>
2025-04-13 20:21:05 +1000

    update

--- RE_website/src/components/static-image.tsx
+++ RE_website/src/components/static-image.tsx
@@ -1,3 +1,5 @@
+import classes from "./static-image.module.scss";
+
 export function StaticImage() {
-  return <img src="map.png" alt="Map" className="map-image" />;
+  return <img src="map.png" alt="Map" className={classes["map-image"]} />;
 }
